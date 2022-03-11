fd1fe713a9dbde11e42445cdd5abd8014c1a6036
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-03-10 21:36:25 -0800

    code fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-int putchar("Programming is like building a multilingual puzzle \n") return (0);
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Returns 0 at the end
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle \n");
+	return (0);
+}
+
