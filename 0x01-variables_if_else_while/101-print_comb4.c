4293c59686dc7c71bf1e044a79ac1615756bded9
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-03-12 01:50:52 -0800

    prints 3 gigits with no repeatition

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/**
+ * main - prints combo of 3 digits without repetition
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 9; y++)
+		{
+			for (z = 0; z < 10; z++)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(z + '0');
+
+				if (x == 7 && y == 8 && z == 9)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
