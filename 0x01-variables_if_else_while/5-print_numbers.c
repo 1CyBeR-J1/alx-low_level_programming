e6676a93198e0f4e69c115a6fea7def66d99f3da
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-03-11 20:55:18 -0800

    prints single digit from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - prints all single digit from 0
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
