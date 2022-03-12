8668ba99affb9d55cdcac725d4b64b0c9d1ae935
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-03-12 05:02:53 -0800

    combination of 2 two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,8 +16,11 @@ int main(void)
 		{
 			putchar(x + '0');
 			putchar(y + '0');
+			putchar(' ');
+			putchar(x + '0');
+			putchar(y + '0');
 
-			if (x == 9 && y == 9)
+			if (x == 98 && y == 99)
 			{
 				break;
 			}
