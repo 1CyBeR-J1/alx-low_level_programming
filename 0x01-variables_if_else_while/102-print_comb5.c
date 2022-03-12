9275f40d101587a71bdd122c611f28307752986f
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-03-12 14:15:28 -0800

    code fixed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,17 +8,23 @@
 
 int main(void)
 {
-	int i, j;
+	int i, j, k, l;
 
 	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
 		{
-			putchar(i + '0');
-			putchar(i + '0');
-			putchar(' ');
-			putchar(j + '0');
-			putchar(j + '0');
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+				}
+			}
 		}
 	}
 	putchar('\n');
