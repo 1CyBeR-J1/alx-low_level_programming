9942beb20b031969d90026972e40f8d13d2fb5a2
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-03-12 00:38:38 -0800

    code fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,17 +13,17 @@ int main(void)
 
 	for  (i = 0; i < 10; i++)
 	{
-		putchar(i + '0');
-	}
 		for (j = 0; j < 10; j++)
 		{
 			if (j != i)
 			{
+				putchar(i + '0');
 				putchar(j + '0');
 				putchar(',');
 				putchar(' ');
 			}
 		}
+	}
 	putchar('\n');
 	return (0);
 }
