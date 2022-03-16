32e3cfe49fb0ef435dbb90c0903a40631a9d8f4d
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-03-15 20:24:44 -0700

    print alphabeths

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alp
+
+	while (alp = 'a'; alp <= 'z'; alp++)
+	{
+		_putchar('%c', alp)
+	}
+	_putchar("\n")
+}
