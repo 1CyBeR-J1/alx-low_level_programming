fca8f26aff1ef66ccad5deed457f0ef6ed599bf9
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-03-16 19:30:50 -0700

    100 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints table of 100
+ * @n: is the integer put in
+ *
+ */
+
+
+void print_times_table(int n)
+{
+	int num, mul, prd;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mul = 1; mul <= n; mul++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prd = num * mul;
+
+				if (prd <= 9 || prd <= 99)
+				{
+					_putchar(' ');
+				}
+
+				if (prd >= 100)
+				{
+					_putchar((prd / 100) + '0');
+					_putchar((prd / 10) % 10 + '0');
+				}
+
+				else if (prd <= 99 && prd >= 10)
+				{
+					_putchar((prd / 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+
+}
