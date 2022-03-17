ff788e3288773436ece1d37f571e7fb22881dea5
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-03-16 18:09:02 -0700

    code fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,18 +10,12 @@
 
 void print_to_98(int n)
 {
-	for (n <= 0; n <= 98; n++)
+	for (n <= 98; n <= 98; n++)
 	{
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
+		printf("%d, ", n);
 	}
 	for (n >= 98; n >= 98; n--)
 	{
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
+		printf("%d, ", n);
 	}
-	_putchar('\n');
-	return (0);
 }
