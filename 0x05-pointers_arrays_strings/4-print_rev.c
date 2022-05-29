ca67ef55b9bfb43c1b7bb6ba64c502d57aa18569
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-05-29 09:34:41 -0700

    prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string, in reverse
+ * @s: the string
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int length;
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
