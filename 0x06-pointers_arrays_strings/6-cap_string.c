cdbbc909913c17c8527e7e837307ed3e1061dd0d
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-05-31 16:18:56 -0700

    capitalizes all stings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -21,9 +21,9 @@ char *cap_string(char *s)
 			s[i] = s[i] - 32;
 		}
 		else if ((s[0] >= 97 && s[0] <= 122))
-				{
-					s[0] = s[0] - 32;
-				}
+		{
+			s[0] = s[0] - 32;
+		}
 		else
 		{
 			s[i] = s[i];
