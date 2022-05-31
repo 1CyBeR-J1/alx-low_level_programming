252f9bcc6ad3380e02262ebbefe1d9af9cc358c8
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-05-31 14:19:47 -0700

    copies two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int result = 0;
 
-	while (*s1 == *s2 && *s1 != '\0)
+	while (*s1 == *s2 && *s1 != '\0')
 	{
 		s1++;
 		s2++;
 	}
-	
+
 	if(s1 != s2)
 		result = *s1 - *s2;
 	return (result);
