9dce491447cae5fbdcd8d48a9d0670e42539369f
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-05-31 04:50:11 -0700

    to print a string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse strong
+ * @s: string to reverse
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j= 0;
+	char tmp;
+
+	while (*(s + i) != '\0')
+		i += 1;
+	i -= 1;
+
+	while (j < 1)
+	{
+		tmp = s[i]
+		s[i] = s[j];
+		s[j] = tmp;
+		j++;
+		i--;
+	}
+}
