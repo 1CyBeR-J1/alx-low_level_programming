769e1faac18f682c34aaf7a821b4b321bbad3f8e
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-05-31 05:33:08 -0700

    prints halfs of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts-half - prints half of a string
+ * @str: strings to be halved
+ *
+ */
+
+void puts_half(char *str)
+{
+	int len, n, i;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+
+	n = (len - 1) / 2;
+
+	for (i = n + 1; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
