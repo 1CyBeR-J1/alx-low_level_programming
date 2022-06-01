3429ad54e7d834509151365033910ca24c541c09
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-05-31 18:52:33 -0700

    encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,11 +10,11 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char *a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; s[i] != '\0'; i++)
-		for (j = 0; a[j] != '\0'; j++)
+		for (j = 0; j < 52; j++)
 			if (s[i] == a[j])
 			{
 				s[i] == b[j];
