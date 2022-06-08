1654379321c5c468facb2c0693eabc1032141434
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-06-08 07:11:46 -0700

    Check if Prime Number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -29,7 +29,7 @@ int getNPrim(int n, int i)
 
 int is_prime_number(int n)
 {
-        if (n <= 1)
-                return (0);
-        return (getNPrim(n, 2));
+	if (n <= 1)
+		return (0);
+	return (getNPrim(n, 2));
 }
