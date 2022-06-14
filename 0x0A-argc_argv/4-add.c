624870c706387eb3dae9801186ab2cde509cdee8
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-06-13 20:09:38 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - program that adds positive numbers
@@ -18,7 +19,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		print ("Error\n");
+		printf("Error\n");
 
 		return (1);
 	}
