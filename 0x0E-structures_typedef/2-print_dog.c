f5ecb78e75e480dd2ad06652233bd8fba8a20381
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-06-29 09:13:36 -0700

    fix on struct example

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 
 	else
 		printf("Name: %s\n", d->name);
-		printf("Name: %s\n", d->age);
+		printf("Name: %.2f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
