b57a45766bcadd014d1da47e78c024751f9385b7
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-07-25 04:15:08 -0700

    Variable funtions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <variadic_functions.h>
+
+/**
+ * print_strings - function that prints strings, followed by a new line
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ *
+ * Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(list, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+
+		if (i < n - 1 && seperator)
+			printf("%s", seperator);
+	}
+	va_end(list);
+	printf("\n")
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include <variadic_functions>
+#include <stdio.h>
+
+/**
+ * print_all - function that prints anything.
+ * @format: list of types of arguments passed to the function
+ * Return: nothing: nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	char *s, *sep = "";
+	int i = 0;
+	va_list args_list;
+
+	va_start(args_list, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(args_list, int));
+					break;
+				case 'i':
+					printf("%s%i", sep, va_arg(args_list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(args_list, int));
+					break;
+				case 's':
+					s = va_arg(args_list, char *);
+					if (!s)
+						s = "(nil)";
+					printf("%s%s, sep, s);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(args_list);
+}
