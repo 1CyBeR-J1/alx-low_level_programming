43ef25e55727d1603f357d71a18a731fdc86f488
1CyBeR-J1 <marvellousojeleke@yahoo.com>
2022-08-22 21:56:40 -0700

    Examples on bits manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - function that checks the endianness.
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int num = 1;
+	char *endian = (char *)&num;
+
+	return ((int)*endian);
+}
