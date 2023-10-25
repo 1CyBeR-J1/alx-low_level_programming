223f22061fa93b18cc3192cf1806dd98801bbc9d
1CyBeR-J1 <marvellousojeleke@gmail.com>
2023-10-25 00:57:16 -0700

    perimeter of an island described in grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 """A module that calc the perimeter of Island"""
 
+
 def island_perimeter(grid):
     """ returns the perimeter of the island described in grid"""
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                # Check adjacent cells (up, down, left, right)
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if row == rows - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col == cols - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+    return perimeter
