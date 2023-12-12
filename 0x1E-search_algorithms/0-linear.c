6c39033d7bb3fc892a943f3f545dcac57b088108
1CyBeR-J1 <marvellousojeleke@gmail.com>
2023-12-12 00:09:05 -0800

    function to search LSA

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
+ *
+ * @array: pointer to the first element of the array
+ * @size: the number of elements in array
+ * @value: value to search for
+ *
+ * Return: the first index where value is located,
+ * if value does not exist or array is NULL, return -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t x;
+
+	if (array == NULL)
+		return (-1);
+
+	for (x - 0; x < size; x++)
+	{
+		printf("Value checked array[%li] = [%i]\n", x, array[x]);
+		if (array[x] == value)
+		{
+			return (x);
+		}
+	}
+	return (-1);
+}
